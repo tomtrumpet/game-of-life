348b9471221675710b3b7f638b2190961473e068
Tom Austin <thomasaustin@gmail.com>
2017-10-18 09:30:48 +0100

    Enumerate with foreach

--- src/board-tick.js
+++ src/board-tick.js
@@ -4,25 +4,25 @@ import grid from './helpers/grid'
 exports.tick = (board) => {
   let changes = grid.createGrid(board.length, board[0].length)
 
-  for (let r = 0; r < board.length; r++) {
-    for (let c = 0; c < board[0].length; c++) {
+  board.forEach((row, r) => {
+    row.forEach((cell, c) => {
       let livingNeighbours = cells.countLivingNeighbours(board, r, c)
 
-      changes[r][c] = shouldLive(board[r][c], livingNeighbours)
-    }
-  }
+      changes[r][c] = shouldLive(cell, livingNeighbours)
+    })
+  })
 
   return makeChanges(board, changes)
 }
 
 const makeChanges = (board, changes) => {
-  for (let r = 0; r < changes.length; r++) {
-    for (let c = 0; c < changes[0].length; c++) {
+  board.forEach((row, r) => {
+    row.forEach((cell, c) => {
       if (changes[r][c] !== null) {
         board[r][c] = changes[r][c]
       }
-    }
-  }
+    })
+  })
 
   return board
 }
